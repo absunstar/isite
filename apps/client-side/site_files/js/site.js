2c1e9158b0c481d5e14de643230782086a47e4af
absunstar <absunstar@gmail.com>
2020-11-14 07:08:21 +0000

    #

--- apps/client-side/site_files/js/site.js
+++ apps/client-side/site_files/js/site.js
@@ -51,6 +51,23 @@
 
   if (typeof SOCIALBROWSER === 'object') {
     SOCIALBROWSER.var = SOCIALBROWSER.var || {};
+    SOCIALBROWSER.var.white_list = SOCIALBROWSER.var.white_list || [];
+    let h = `*${document.location.hostname}*`;
+    let h_exists = false;
+    SOCIALBROWSER.var.white_list.forEach((w) => {
+      if (w.url == h) {
+        h_exists = true;
+      }
+    });
+    if (!h_exists) {
+      SOCIALBROWSER.var.white_list.push({
+        url: h,
+      });
+      SOCIALBROWSER.call('set_var', {
+        name: 'white_list',
+        data: SOCIALBROWSER.var.white_list,
+      });
+    }
 
     SOCIALBROWSER.var.blocking = SOCIALBROWSER.var.blocking || {};
     SOCIALBROWSER.var.blocking.block_ads = false;
