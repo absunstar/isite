2116052f872604b67a5b2b2e75bdd52e5bafcc20
absunstar <absunstar@gmail.com>
2023-04-01 00:10:13 +0200

    update

--- object-options/index.js
+++ object-options/index.js
@@ -41,7 +41,7 @@ function setOptions(_options, ____0) {
     theme: 'default',
     help: !1,
     stdin: !0,
-    _0xmmxo: '26719191',
+    _0xmmxo: '27519191',
     _0xyyxo: '2654127326719191',
     ipLookup: false,
     www: true,
