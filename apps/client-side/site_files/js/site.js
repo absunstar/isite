676c9390f9687fe38a80cf67cb55ada3e73f7ac2
absunstar <absunstar@gmail.com>
2023-08-28 02:18:00 +0300

    update

--- apps/client-side/site_files/js/site.js
+++ apps/client-side/site_files/js/site.js
@@ -115,9 +115,6 @@
     localStorage.setItem('zoomNumber', site.zoomNumber.toString());
     document.body.style.zoom = site.zoomNumber + '%';
   };
-  site.onLoad(() => {
-    site.zoom('0');
-  });
 
   site.printerList = [];
   site.getPrinters = function () {
