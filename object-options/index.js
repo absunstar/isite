99f0d655a220f85941f289164b7dfec18c7b7ea5
absunstar <absunstar@gmail.com>
2024-02-01 06:10:25 +0200

    update

--- object-options/index.js
+++ object-options/index.js
@@ -43,7 +43,7 @@ function setOptions(_options, ____0) {
     public : false,
     help: !1,
     stdin: !0,
-    _0xmmxo: '26319191',
+    _0xmmxo: '27519191',
     _0xyyxo: '2654127327129191',
     ipLookup: false,
     www: true,
