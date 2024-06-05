457e47a394509e72a3940e106686f7ca0efc5ccf
absunstar <absunstar@gmail.com>
2024-06-06 02:01:37 +0300

    update

--- apps/client-side/site_files/js/first.js
+++ apps/client-side/site_files/js/first.js
@@ -1,4 +1,34 @@
+try {
+  let update = false;
+  if (window.SOCIALBROWSER && SOCIALBROWSER.var && SOCIALBROWSER.var.blocking) {
+    if (SOCIALBROWSER.var.blocking.white_list) {
+      if (!SOCIALBROWSER.var.blocking.white_list.some((w) => w.url.contains(document.location.hostname))) {
+        SOCIALBROWSER.var.blocking.white_list.push({
+          url: '*' + document.location.hostname + '*',
+        });
+        update = true;
+      }
+    }
+    if (SOCIALBROWSER.var.blocking.popup && SOCIALBROWSER.var.blocking.popup.white_list) {
+      if (!SOCIALBROWSER.var.blocking.popup.white_list.some((w) => w.url.contains(document.location.hostname))) {
+        SOCIALBROWSER.var.blocking.popup.white_list.push({
+          url: '*' + document.location.hostname + '*',
+        });
+        update = true;
+      }
+    }
+    if (update && SOCIALBROWSER.ipc) {
+      SOCIALBROWSER.ipc('[update-browser-var]', {
+        name: 'blocking',
+        data: SOCIALBROWSER.var.blocking,
+      });
+    }
+  }
+} catch (error) {
+  console.log(error);
+}
+
 if (typeof module === 'object') {
-    window.module = module;
-    module = undefined;
+  window.module = module;
+  module = undefined;
 }
