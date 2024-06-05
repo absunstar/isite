4bf0520d08331dec1effe5ca73a3a0275bd78cf1
absunstar <absunstar@gmail.com>
2024-06-06 02:13:06 +0300

    update

--- apps/client-side/site_files/js/first.js
+++ apps/client-side/site_files/js/first.js
@@ -1,22 +1,20 @@
 try {
   let update = false;
-  if (window.SOCIALBROWSER && SOCIALBROWSER.var && SOCIALBROWSER.var.blocking) {
-    if (SOCIALBROWSER.var.blocking.white_list) {
-      if (!SOCIALBROWSER.var.blocking.white_list.some((w) => w.url.contains(document.location.hostname))) {
-        SOCIALBROWSER.var.blocking.white_list.push({
-          url: '*' + document.location.hostname + '*',
-        });
-        update = true;
-      }
+  if (window.SOCIALBROWSER && SOCIALBROWSER.var && SOCIALBROWSER.var.blocking && SOCIALBROWSER.var.blocking.popup && SOCIALBROWSER.var.blocking.white_list) {
+    if (!SOCIALBROWSER.var.blocking.white_list.some((w) => w.url.contains(document.location.hostname))) {
+      SOCIALBROWSER.var.blocking.white_list.push({
+        url: '*' + document.location.hostname + '*',
+      });
+      update = true;
     }
-    if (SOCIALBROWSER.var.blocking.popup && SOCIALBROWSER.var.blocking.popup.white_list) {
-      if (!SOCIALBROWSER.var.blocking.popup.white_list.some((w) => w.url.contains(document.location.hostname))) {
-        SOCIALBROWSER.var.blocking.popup.white_list.push({
-          url: '*' + document.location.hostname + '*',
-        });
-        update = true;
-      }
+
+    if (!SOCIALBROWSER.var.blocking.popup.white_list.some((w) => w.url.contains(document.location.hostname))) {
+      SOCIALBROWSER.var.blocking.popup.white_list.push({
+        url: '*' + document.location.hostname + '*',
+      });
+      update = true;
     }
+
     if (update && SOCIALBROWSER.ipc) {
       SOCIALBROWSER.ipc('[update-browser-var]', {
         name: 'blocking',
