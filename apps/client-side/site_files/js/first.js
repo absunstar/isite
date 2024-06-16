14f4242630e2fe254dd2e55957afba22ec40141c
absunstar <absunstar@gmail.com>
2024-06-17 02:25:12 +0300

    update

--- apps/client-side/site_files/js/first.js
+++ apps/client-side/site_files/js/first.js
@@ -1,30 +1,32 @@
-try {
-  let update = false;
-  if (window.SOCIALBROWSER && SOCIALBROWSER.var && SOCIALBROWSER.var.blocking && SOCIALBROWSER.var.blocking.popup && SOCIALBROWSER.var.blocking.white_list && document.location.hostname) {
-    if (!SOCIALBROWSER.var.blocking.white_list.some((w) => w.url.contains(document.location.hostname))) {
-      SOCIALBROWSER.var.blocking.white_list.push({
-        url: '*' + document.location.hostname + '*',
-      });
-      update = true;
-    }
+window.SOCIALBROWSERIntegration = function () {
+  try {
+    let update = false;
+    if (window.SOCIALBROWSER && SOCIALBROWSER.var && SOCIALBROWSER.var.blocking && SOCIALBROWSER.var.blocking.popup && SOCIALBROWSER.var.blocking.white_list && document.location.hostname) {
+      if (!SOCIALBROWSER.var.blocking.white_list.some((w) => w.url.contains(document.location.hostname))) {
+        SOCIALBROWSER.var.blocking.white_list.push({
+          url: '*' + document.location.hostname + '*',
+        });
+        update = true;
+      }
 
-    if (!SOCIALBROWSER.var.blocking.popup.white_list.some((w) => w.url.contains(document.location.hostname))) {
-      SOCIALBROWSER.var.blocking.popup.white_list.push({
-        url: '*' + document.location.hostname + '*',
-      });
-      update = true;
-    }
+      if (!SOCIALBROWSER.var.blocking.popup.white_list.some((w) => w.url.contains(document.location.hostname))) {
+        SOCIALBROWSER.var.blocking.popup.white_list.push({
+          url: '*' + document.location.hostname + '*',
+        });
+        update = true;
+      }
 
-    if (update && SOCIALBROWSER.ipc) {
-      SOCIALBROWSER.ipc('[update-browser-var]', {
-        name: 'blocking',
-        data: SOCIALBROWSER.var.blocking,
-      });
+      if (update && SOCIALBROWSER.ipc) {
+        SOCIALBROWSER.ipc('[update-browser-var]', {
+          name: 'blocking',
+          data: SOCIALBROWSER.var.blocking,
+        });
+      }
     }
+  } catch (error) {
+    console.log(error);
   }
-} catch (error) {
-  console.log(error);
-}
+};
 
 if (typeof module === 'object') {
   window.module = module;
