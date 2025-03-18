1a8ee6e1b3bc133292900e9fe95bc5a1118e51d3
absunstar <absunstar@gmail.com>
2025-03-18 22:45:33 +0200

    update

--- apps/security/site_files/js/register.js
+++ apps/security/site_files/js/register.js
@@ -1,7 +1,6 @@
 app.controller('register', function ($scope, $http) {
-
     $scope.busy = !1;
-  
+
     $scope.register = function () {
         $scope.error = '';
         $scope.busy = !0;
@@ -9,23 +8,24 @@ app.controller('register', function ($scope, $http) {
             method: 'POST',
             url: '/api/user/register',
             data: {
-                $encript : '123',
+                $encript: '123',
                 email: site.to123($scope.userEmail),
-                password: site.to123($scope.userPassword)
-            }
-        }).then(function (response) {
-           
-            if (response.data.error) {
-                $scope.error = response.data.error;
+                password: site.to123($scope.userPassword),
+            },
+        }).then(
+            function (response) {
+                if (response.data.error) {
+                    $scope.error = response.data.error;
+                    $scope.busy = !1;
+                }
+                if (response.data.user) {
+                    window.location.href = '/';
+                }
+            },
+            function (err) {
                 $scope.busy = !1;
+                $scope.error = err;
             }
-            if (response.data.user) {
-                window.location.href = '/';
-            }
-        } , function(err){
-            $scope.busy = !1;
-            $scope.error = err;
-        });
-
+        );
     };
 });
